62a81a880df5f8706d31033b9f02b4a59c6f0d1b
marcus <mreynolds746@gmail.com>
2018-03-16 19:29:57 -0700

    v1.0

---
+++ public/script.js
@@ -0,0 +1,243 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    player: [],
+    enemy:[],
+    enemyAttackType:0,
+    name: '',
+    location: 'newPlayer',
+    default_attack: 3,
+    default_maxHp:12,
+    default_defense:0,
+    descriptions: [],
+    //show: 'all',
+    //drag: {},
+  },
+
+   created:function() {
+      // Initialize Descriptions()
+	this.descriptions[0]=''; // teaching basics
+	this.descriptions[1]='Take a rest at Ye Olde Tavern to fully restore your health' // inn
+	this.descriptions[2]='Home of the finest blacksmith in the kingdom' // smithy
+	this.descriptions[3]='Challenge the next tournament contender' // jousting
+
+	this.descriptions[4]='Strengthen your equipment, increasing attack by 1' // attack
+	this.descriptions[5]='Forge stronger armor, reducing damage taken by 1' // defense
+	this.descriptions[6]='Enhance your armor, increasing your max health by 2' // maxHp
+      // Check if a player save file already exists
+      this.getPlayer();
+   },
+
+  computed: {
+    recentAttack:function() {
+	if (this.enemyAttackType===3)
+	   return "images/lance.png"
+	else if (this.enemyAttackType==2)
+	  return "images/shield.png"
+	else if (this.enemyAttackType==1)
+	  return "images/sword.png"
+	else
+	  return "images/unknown.png";
+    },
+  },
+  methods: {
+
+   getPlayer: function() {
+     axios.get("/api/player").then(response => {
+	this.player=response.data;
+	this.name=this.player.name;
+	this.location=this.player.location;
+	console.log("Name: "+this.player.name);
+	console.log("Location: "+this.player.location);
+	console.log("Hp: "+this.player.currentHp);
+     if (this.player.name.length>0) {
+         this.location=this.player.location;
+	 this.getEnemy();
+	}
+     return true;
+    }).catch(err => {});
+
+
+   },
+
+  getEnemy:function() {
+	axios.get("/api/enemy").then(response => {
+     this.enemy=response.data;
+     return true;
+    }).catch(err => {});
+   },
+
+  newPlayer: function () {
+	// Make sure a name has been entered
+	if (this.name.length>0) {
+	   this.name= this.name.charAt(0).toUpperCase() + this.name.slice(1);
+	// Initialize player stats
+	axios.post("/api/player", {
+	   name:this.name,
+	   defense:this.default_defense,
+	   attack:this.default_attack,
+	   maxHp:this.default_maxHp,
+	   currentHp:this.default_maxHp,
+	   battlesWon:0,
+	   gold:0,
+	   image:'', // finish later
+	   location:'town',
+	}).then(response => {
+	//this.text="";
+	   this.getPlayer();
+	   return true;
+	}).catch(err => { });
+
+	// Initialize first enemy
+	axios.post("/api/enemy", {
+	  name:'Sir Evil', // At some point, use random names
+	  defense:0,
+	  attack:this.default_attack-1,
+	  maxHp:this.default_maxHp/2,
+	  currentHp:this.default_maxHp/2,
+	  image:'', // will finish later
+       }).then(response => {
+	   this.getEnemy();
+	   return true;
+	}).catch(err => { });
+
+	}
+   },
+
+   updatePlayer: function () {
+	axios.post("/api/player", {
+	   name: this.name,
+	   defense: this.player.defense,
+	   attack: this.player.attack,
+	   maxHp: this.player.maxHp,
+	   currentHp: this.player.currentHp,
+	   battlesWon: this.player.battlesWon,
+	   gold: this.player.gold,
+	   image: this.player.image,
+	   location: this.location,
+	}).then(response => {
+	this.getPlayer();
+	   return true;
+	}).catch(err => { });
+   },
+
+   updateEnemy: function () {
+	axios.post("/api/enemy", {
+	   name:this.enemy.name,
+	   defense:this.enemy.defense,
+	   attack:this.enemy.attack,
+	   maxHp:this.enemy.maxHp,
+	   currentHp:this.enemy.currentHp,
+	   image:this.player.image,
+	}).then(response => {
+	   return true;
+	}).catch(err => { });
+   },
+
+   retire:function() {
+	axios.delete("/api/player").
+	then(response => {
+	  this.name='';
+	  //this.getPlayer();
+	  this.location='newPlayer';
+	  this.updatePlayer();
+	  return true;
+	}).catch(err => {});
+    },
+
+   goInn:function() {
+	this.location='inn';
+	this.updatePlayer();
+	},
+   goSmithy:function() {
+	this.location='smithy';
+	this.updatePlayer();
+	},
+   goJousting:function() {
+	this.location='jousting';
+	this.updatePlayer();
+	},
+   goTown:function() {
+	this.location='town';
+	this.updatePlayer();
+	},
+
+   attack:function(type) {
+	// play random sound effect
+	let attackSound=1+Math.floor(Math.random()*4);
+	let audio = new Audio('sound/attack'+attackSound+'.mp3');
+	audio.play();
+
+	// 1=sword, 2=shield, 3=lance
+	let enemyAttack=1+Math.floor(Math.random() * 3);
+	this.enemyAttackType=enemyAttack;
+	if (type==1 && enemyAttack==2)
+		this.enemy.currentHp=this.enemy.currentHp-this.player.attack;
+	else if (type==2 && enemyAttack==3)
+		this.enemy.currentHp=this.enemy.currentHp-this.player.attack;
+	else if (type==3 && enemyAttack==1)
+		this.enemy.currentHp=this.enemy.currentHp-this.player.attack;
+	else if (type==2 && enemyAttack==1)
+		this.player.currentHp=this.player.currentHp-this.enemy.attack;
+	else if (type==3 && enemyAttack==2)
+		this.player.currentHp=this.player.currentHp-this.enemy.attack;
+	else if (type==1 && enemyAttack==3)
+		this.player.currentHp=this.player.currentHp-this.enemy.attack;
+
+	this.updateEnemy();
+
+	if (this.player.currentHp<=0) {
+	   this.location="death";
+	}
+	else if (this.enemy.currentHp<=0) {
+		this.location="victory";
+		this.player.battlesWon++;
+		this.player.gold+=5;
+		this.enemyAttackType=0;
+	
+		let attackmod=this.player.battlesWon%2;
+		// create a new, stronger enemy
+		axios.post("/api/enemy", {
+	 	name:'Sir Evil'+this.player.battlesWon, // At some point, use random names
+	  	defense:0,
+	  	attack:this.enemy.attack+attackmod,
+	  	maxHp:this.enemy.maxHp+1,
+	 	currentHp:this.enemy.maxHp+1,
+	  	image:'', // will finish later
+       		}).then(response => {
+	   	this.getEnemy();
+	   	return true;
+		}).catch(err => { });
+	}
+	this.updatePlayer();
+
+	},
+
+   rest:function() {
+	if (this.player.gold>=2 && this.player.currentHp<this.player.maxHp) {
+	  this.player.gold-=2;
+	  this.player.currentHp=this.player.maxHp;
+	  this.location='town';
+	  this.updatePlayer();
+	}
+  },
+  upgrade:function(type) {
+	if (type==='attack' && this.player.gold>=5) {
+		this.player.attack++;
+		this.player.gold-=5;
+		this.updatePlayer();
+	}
+	else if (type==='maxHp' && this.player.gold>=4) {
+		this.player.maxHp+=2;
+		this.player.currentHp+=2;
+		this.player.gold-=4;
+		this.updatePlayer();
+	}
+	else if (type==='defense' && this.player.gold>=10) {
+		this.player.defense++;
+		this.player.gold-=10;
+		this.updatePlayer();
+	}
+	}
+}
+});
---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+// Accept incoming POST requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static('public'))
+
+let player = {};
+let enemy={};
+//let id = 0;
+
+app.get('/api/player', (req, res) => {
+  res.send(player);
+});
+
+app.get('/api/enemy', (req, res) => {
+  res.send(enemy);
+});
+
+app.post('/api/player', (req, res) => {
+  player = {name:req.body.name, attack: req.body.attack,
+	defense:req.body.defense, currentHp:req.body.currentHp,
+	maxHp:req.body.maxHp, battlesWon:req.body.battlesWon, gold:req.body.gold, image:req.body.image,location:req.body.location};
+  res.send(player);
+});
+
+app.post('/api/enemy', (req, res) => {
+  enemy = {name:req.body.name, attack: req.body.attack,
+	defense:req.body.defense, currentHp:req.body.currentHp,
+	maxHp:req.body.maxHp, image:req.body.image};
+  res.send(enemy);
+});
+/*
+app.put('/api/player/:stat', (req,res) => {
+	let id = parseInt(req.params.id);
+ 	let itemsMap = items.map(item => { return item.id; });
+  	let index = itemsMap.indexOf(id);
+  	let item = items[index];
+ 	item.completed = req.body.completed;
+  	item.text = req.body.text;
+	item.priority=req.body.priority;
+  	// handle drag and drop re-ordering
+  	if (req.body.orderChange) {
+    	   let indexTarget = itemsMap.indexOf(req.body.orderTarget);
+    	   items.splice(index,1);
+    	   items.splice(indexTarget,0,item);
+  	}
+  res.send(item);
+});
+*/
+
+app.delete('/api/player',(req,res) => {
+  player={};
+  enemy={};
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => console.log('Server listening on port 3000!'))
